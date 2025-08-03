ee0af05fc9de904e8bae7a6733a5b613fa534ca7
buckbeak0 <148715848+buckbeak0@users.noreply.github.com>
2025-08-03 19:29:23 +0530

    How to store values

---
+++ Basics/DataTypes/Primitive.java
@@ -0,0 +1,18 @@
+public class Primitive {
+    public static void main(String[] args) {
+        char c = 'a';
+        int i = 1234;
+        float f = 123.45f;
+        double d = 1234.5678;
+        long l = 23456785674322L;
+        boolean b = true; //or false only
+
+        System.out.println("char = "+c); 
+        System.out.println("int = "+i);
+        System.out.println("float = "+f);
+        System.out.println("double = "+d);
+        System.out.println("long = "+l);
+        System.out.println("bool = "+b);
+    
+    }    
+}
